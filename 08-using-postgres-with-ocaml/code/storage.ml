bbfc3c4aa720d601da784ea7b7bb205575e53567
EduardoRFS <theeduardorfs@gmail.com>
2021-04-19 19:26:49 +0300

    #8: integrate with postgres

--- 08-using-postgres-with-ocaml/code/storage.ml
+++ 08-using-postgres-with-ocaml/code/storage.ml
@@ -1,26 +1,71 @@
+(* types *)
+module type DB = Rapper_helper.CONNECTION
+
+exception Query_failed of string
+
 type message = { user_name : string; body : string } [@@deriving yojson]
 
+type message_stored = { id : string; user_name : string; body : string }
+
+(* helpers *)
 let ( let* ) = Lwt.bind
 
-let database_file = "messages.json"
+(* setup of database pool *)
 
+let connection_url = "postgresql://postgres:password@localhost:5432"
+
+let pool =
+  match Caqti_lwt.connect_pool ~max_size:10 (Uri.of_string connection_url) with
+  | Ok pool -> pool
+  | Error error -> failwith (Caqti_error.show error)
+
+let dispatch f =
+  let* result = Caqti_lwt.Pool.use f pool in
+  match result with
+  | Ok data -> Lwt.return data
+  | Error error -> Lwt.fail (Query_failed (Caqti_error.show error))
+
+(* running migrations *)
+let ensure_table_exists =
+  [%rapper
+    execute
+      {sql|
+        CREATE TABLE IF NOT EXISTS messages (
+          id VARCHAR PRIMARY KEY NOT NULL,
+          user_name VARCHAR,
+          body VARCHAR
+        );
+      |sql}]
+    ()
+
+let () = dispatch ensure_table_exists |> Lwt_main.run
+
+(* queries *)
 let read_all_messages () =
-  Lwt_io.with_file ~mode:Input database_file (fun input_channel ->
-      let* database_string =
-        Lwt_io.read_lines input_channel |> Lwt_stream.to_list
-      in
-      let database_json =
-        Yojson.Safe.from_string (String.concat "\n" database_string)
-      in
-      match [%of_yojson: message list] database_json with
-      | Ok messages -> Lwt.return messages
-      | Error error -> raise (Invalid_argument error))
+  let read_all =
+    [%rapper
+      get_many
+        {sql|
+          SELECT @string{id}, @string{user_name}, @string{body}
+          FROM messages
+        |sql}
+        record_out]
+      ()
+  in
+  let* messages = dispatch read_all in
+  messages
+  |> List.map (fun { user_name; body; _ } -> { user_name; body })
+  |> Lwt.return
 
-let insert_message message =
-  let* messages = read_all_messages () in
-  let messages = message :: messages in
-  Lwt_io.with_file ~mode:Output database_file (fun output_channel ->
-      let messages_string =
-        messages |> [%to_yojson: message list] |> Yojson.Safe.pretty_to_string
-      in
-      Lwt_io.write output_channel messages_string)
+let insert_message ({ user_name; body } : message) =
+  let insert =
+    [%rapper
+      execute
+        {sql|
+          INSERT INTO messages
+          VALUES(%string{id}, %string{user_name}, %string{body})
+        |sql}
+        record_in]
+  in
+  let id = Uuidm.create `V4 |> Uuidm.to_string in
+  dispatch (insert { id; user_name; body })
--- 08-using-postgres-with-ocaml/code/storage.mli
+++ 08-using-postgres-with-ocaml/code/storage.mli
@@ -1,5 +1,7 @@
 type message = { user_name : string; body : string } [@@deriving yojson]
 
+exception Query_failed of string
+
 val read_all_messages : unit -> message list Lwt.t
 
 val insert_message : message -> unit Lwt.t
