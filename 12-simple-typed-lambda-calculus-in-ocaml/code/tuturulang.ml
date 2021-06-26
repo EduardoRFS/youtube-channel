921bae0b1ae8117f3f3ff2c620416b395706dd5b
EduardoRFS <theeduardorfs@gmail.com>
2021-06-26 18:23:58 -0300

    #12: add typer

--- 12-simple-typed-lambda-calculus-in-ocaml/code/tuturulang.ml
+++ 12-simple-typed-lambda-calculus-in-ocaml/code/tuturulang.ml
@@ -1,51 +1,86 @@
 module Context = Map.Make (String)
 
-type expr =
-  | Variable of string
-  | Abstraction of { param : string; body : expr }
-  | Application of { funct : expr; argument : expr }
+exception Type_error
 
-type value =
-  | Closure of { context : value Context.t; param : string; body : expr }
-  | Native of (value -> value)
+module Typ = struct
+  type typ = TArrow of { param_typ : typ; body_typ : typ } | TInt
+  [@@deriving show { with_path = false }]
 
-let rec interp context expr =
-  match expr with
-  | Variable name -> Context.find name context
-  | Abstraction { param; body } -> Closure { context; param; body }
-  | Application { funct; argument } -> (
-      let argument = interp context argument in
-      match interp context funct with
-      | Closure { context; param; body } ->
-          interp (Context.add param argument context) body
-      | Native f -> f argument)
+  let rec equal a b =
+    match (a, b) with
+    | a, b when a == b -> true
+    | TInt, TInt -> true
+    | ( TArrow { param_typ = param_a; body_typ = body_a },
+        TArrow { param_typ = param_b; body_typ = body_b } ) ->
+        equal param_a param_b && equal body_a body_b
+    | _ -> false
+end
 
-let f =
-  Abstraction
-    {
-      param = "f";
-      body =
-        Application
-          {
-            funct = Variable "f";
-            argument =
-              Application
-                {
-                  funct = Variable "print_hello_world";
-                  argument = Variable "f";
-                };
-          };
-    }
+module Expr = struct
+  open Typ
 
-(* ((λf.(f (print_hello_world f))) (λf.(f (print_hello_world f)))) *)
-let code = Application { funct = f; argument = f }
+  type expr =
+    | Int of int
+    | Variable of string
+    | Abstraction of { param : string; param_typ : typ; body : expr }
+    | Application of { funct : expr; argument : expr }
+end
 
-let initial_context =
-  Context.empty
-  |> Context.add "print_hello_world"
-       (Native
-          (fun v ->
-            print_endline "hello world";
-            v))
+module Infer = struct
+  open Typ
+  open Expr
 
-let _x = interp initial_context code
+  let rec infer context expr =
+    match expr with
+    | Int _ -> TInt
+    | Variable name -> (
+        match Context.find_opt name context with
+        | Some typ -> typ
+        | None -> raise Type_error)
+    | Abstraction { param; param_typ; body } ->
+        let context = Context.add param param_typ context in
+        let body_typ = infer context body in
+        TArrow { param_typ; body_typ }
+    | Application { funct; argument } -> (
+        let funct_typ = infer context funct in
+        let argument_typ = infer context argument in
+        match funct_typ with
+        | TArrow { param_typ; body_typ } when Typ.equal param_typ argument_typ
+          ->
+            body_typ
+        | _ -> raise Type_error)
+end
+
+module Value = struct
+  open Expr
+
+  type value =
+    | VInt of int
+    | VClosure of { context : value Context.t; param : string; body : expr }
+    | VNative of (value -> value)
+
+  let value_pp fmt value =
+    match value with
+    | VInt n -> Format.fprintf fmt "VInt %d" n
+    | VClosure _ -> Format.fprintf fmt "VClosure"
+    | VNative _ -> Format.fprintf fmt "VNative"
+end
+
+module Interp = struct
+  open Expr
+  open Value
+
+  let rec interp context expr =
+    match expr with
+    | Int n -> VInt n
+    | Variable name -> Context.find name context
+    | Abstraction { param; param_typ = _; body } ->
+        VClosure { context; param; body }
+    | Application { funct; argument } -> (
+        let argument = interp context argument in
+        match interp context funct with
+        | VClosure { context; param; body } ->
+            interp (Context.add param argument context) body
+        | VNative f -> f argument
+        | VInt _ -> raise Type_error)
+end
